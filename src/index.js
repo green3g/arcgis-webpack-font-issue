7a1c402d3e8a421f580e11b1038f40bcc86dcfc7
Gregg Roemhildt <groemhildt@wsbeng.com>
2018-05-23 08:51:57 -0500

    font-awesome

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './styles.less';
 import 'esri/themes/light/main.scss';
 import 'esri/themes/light/view.scss';
+import 'font-awesome/css/font-awesome.css';
 
+import './esriConfig';
 import Map from 'esri/Map';
 import View from 'esri/views/MapView';
 
